6c0740720209c92e60025e439aaad3814062839f
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2025-02-04 20:30:51 -0300

    Improvements
    * move metrics to /metrics
    * add a static /
    * set content-type properly
    * add a README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rust SQL Exporter
+
+This is a prometheus exporter that does queries in a PostgreSQL or SQL Server databases and expose as metrics.
+
+## Why another exporter?
+
+An exporter is a short project and I found it a good way to learn rust.
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::fs;
 
-use axum::{response::Json, routing::get, Router};
+use axum::{
+    http::header,
+    response::{Html, IntoResponse, Json},
+    routing::get,
+    Router,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -12,7 +17,9 @@ struct Query {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/metrics", get(handler_metrics))
+        .route("/", get(handler_main));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -23,9 +30,32 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Json<Value> {
+async fn handler_main() -> Html<&'static str> {
+    Html(
+        "<html>
+<head><title>SQL Exporter</title></head>
+  <body>
+    <h1>SQL Exporter</h1>
+    <p><a href='/metrics'>Metrics</a></p>
+  </body>
+</html>",
+    )
+}
+
+async fn handler_metrics() -> impl IntoResponse {
+    (
+        [(
+            header::CONTENT_TYPE,
+            "text/plain; version=0.0.4; charset=utf-8; escaping=values",
+        )],
+        body(),
+    )
+}
+
+fn body() -> String {
     let config: String = fs::read_to_string("sql.yaml").unwrap();
 
     let queries: Vec<Query> = serde_yaml::from_str(&config).unwrap();
-    Json(serde_json::to_value(&queries).expect("Error!"))
+    let json = serde_json::to_value(&queries).expect("Error in to_value");
+    serde_json::to_string_pretty(&json).expect("Error in to_string_pretty")
 }
